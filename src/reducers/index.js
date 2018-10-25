e32a7677841621cfdcb8c9cddbf360839ba48b96
Darii Petru <pdarii@pentalog.fr>
2018-10-25 18:22:27 +0300

    begin inspect

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+// import todos from './todos'
+import visibility from './visibility'
+
+const rootReducer = combineReducers({
+  visibility
+});
+
+export default rootReducer
---
+++ src/reducers/visibility.js
@@ -0,0 +1,9 @@
+const SHOW_ALL = 'show_all'
+
+const visibilityFilter = (state = SHOW_ALL, action) => {
+  console.log('aaaaaaaaaaaaaaa');
+  console.log({ action });
+  return state
+}
+
+export default visibilityFilter
