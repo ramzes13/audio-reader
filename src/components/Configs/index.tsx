97804023ca16aa318f24e42fcbfc3938890e5498
Darii Petru <pdarii@pentalog.fr>
2018-12-28 18:18:06 +0200

    begin add menu

--- src/components/Configs/index.tsx
+++ src/components/Configs/index.tsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import classNames from 'classnames';
 import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import green from '@material-ui/core/colors/green';
+import { withStyles } from '@material-ui/core/styles';
 
-import ConfigsActive from './ConfigsActive';
 import { ReducersConfigStore } from '../../reducers/configs';
 import { ReducersInterface } from '../../reducers';
 import { actions } from '../../actions';
-import UiGenericContainer from '../../ui/GenericComponent';
 
-const Configs = ({ active, toggleActive }: any) => (
-  <UiGenericContainer active={active} toggleActive={toggleActive}>
-    <Button variant="contained" color="primary">Advanced Options</Button>
-  </UiGenericContainer>
-);
+const styles = (theme: any) => ({
+  fab: {
+    margin: '0px',
+    top: 'auto',
+    right: '20px',
+    bottom: '20px',
+    left: 'auto',
+    position: 'fixed' as 'fixed',
+  },
+  fabMenu: {
+    color: theme.palette.common.white,
+    backgroundColor: green[500],
+  },
+});
+
+class Configs extends React.Component<any, any>{
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Fab className={classNames(classes.fab, classes.fabMenu)} >
+        <AddIcon />
+      </Fab>
+    )
+  };
+}
 
 const mapStateToProps = (state: ReducersInterface): ReducersConfigStore => ({ ...state.configs })
 
@@ -23,15 +47,9 @@ const mapDispatchToProps = (dispatch: any) => ({
   }
 })
 
-Configs.propTypes = {
-  active: PropTypes.bool.isRequired,
-  toggleActive: PropTypes.func.isRequired
-}
-
-
-export default connect(
+const component = connect(
   mapStateToProps,
   mapDispatchToProps
 )(Configs)
 
-
+export default withStyles(styles, { withTheme: true })(component);
