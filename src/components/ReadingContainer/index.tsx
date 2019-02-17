bd178b4f996df512cc12b7bde19e2589bfd95e39
darii petru <darii.petru@gmail.com>
2019-02-17 16:38:56 +0200

    epubjs scroll to current edit range

--- src/components/ReadingContainer/index.tsx
+++ src/components/ReadingContainer/index.tsx
@@ -20,23 +20,6 @@ type Props = ReducersReadingStore & DispatchProps;
 class ReadingContainer extends React.Component<Props, any> {
   book: any;
   rendition: any;
-
-  // static propTypes = {
-  //   selectedRegion: PropTypes.object.isRequired,
-  //   annotationType: PropTypes.string.isRequired
-  // }
-
-  // constructor(props: any) {
-  //   super(props);
-  //   this.state = {
-  //     selectedRegion: null,
-  //   };
-
-  //   this.nextPage = this.nextPage.bind(this);
-  //   this.prevPage = this.prevPage.bind(this);
-  //   this.prepareMeta = this.prepareMeta.bind(this);
-  // }
-
   nextPage() {
     this.book.package.metadata.direction === "rtl" ? this.rendition.prev() : this.rendition.next();
     // console.log('nextPage')
@@ -92,64 +75,9 @@ class ReadingContainer extends React.Component<Props, any> {
     this.rendition.on("selected", (cfiRange: any, contents: any) => {
 
       this.prepareMeta(cfiRange);
-      // this.book.getRange(cfiRange).then((range) => {
-      //   if (range) {
-      //     this.props.onChangeMeta({ cfiRange, range });
-      //   }
-      // });
-      // console.log(this.rendition.annotations)
-
-      // this.rendition.annotations.add(this.props.annotationType, cfiRange, {}, (e: any) => {
-      //   console.log("this.rendition.annotations", this.rendition.annotations);
-      //   console.log({ cfiRange });
-      // });
-
-      // this.rendition.annotations.mark(cfiRange, {}, (e: any) => {
-      //   console.log("this.rendition.annotations", this.rendition.annotations);
-      //   console.log({ cfiRange });
-      // });
-      // contents.window.getSelection().removeAllRanges();
-
     });
 
-    // this.rendition.themes.default({
-    //   '::selection': {
-    //     'background': 'rgba(255,255,0, 0.3)'
-    //   },
-    //   '.epubjs-hl': {
-    //     'fill': 'yellow', 'fill-opacity': '0.3', 'mix-blend-mode': 'multiply'
-    //   }
-    // });
-
   }
-
-  // shouldComponentUpdate(nextProps: any, nextState: any) {
-  // if (!this.props.selectedRegion) {
-  //   if (this.state.selectedRegion) {
-  //     this.rendition.annotations.remove(this.state.selectedRegion.bookMeta.cfiRange);
-  //     this.setState(() => {
-  //       return { selectedRegion: null }
-  //     });
-  //   }
-  // } else {
-  //   if (this.state.selectedRegion) {
-  //     if (this.props.selectedRegion.id !== this.state.selectedRegion.id) {
-  //       this.rendition.annotations.add(this.props.annotationType, this.props.selectedRegion.bookMeta.cfiRange, {}, (e) => { });
-  //       this.rendition.annotations.remove(this.state.selectedRegion.bookMeta.cfiRange, (e) => { });
-  //       this.setState(() => {
-  //         return { selectedRegion: this.props.selectedRegion }
-  //       });
-  //     }
-  //   } else {
-  //     this.rendition.annotations.add(this.props.annotationType, this.props.selectedRegion.bookMeta.cfiRange, {}, (e) => { });
-  //     this.setState(() => {
-  //       return { selectedRegion: this.props.selectedRegion }
-  //     });
-  //   }
-  // }
-
-  // return false;
-  // }
 
   clearAnnotations() {
     const _this = this;
@@ -168,6 +96,7 @@ class ReadingContainer extends React.Component<Props, any> {
       console.log(this.props.selectedRegion)
       const { cfiRange } = this.props.selectedRegion;
 
+      this.rendition.display(cfiRange); //scroll to selection
       this.rendition.annotations.add(this.props.annotationType, cfiRange, {}, (e: any) => {
         console.log("this.rendition.annotations", this.rendition.annotations);
         console.log({ cfiRange });
